9b69f3ba07fe465d7b3fb3c735cafd4aafa7701e
nisitin <seiya110307@icloud.com>
2022-04-25 22:22:10 +0900

    chore: マージしたものをコミット

--- src/components/screens/BlogListScreens/index.tsx
+++ src/components/screens/BlogListScreens/index.tsx
@@ -6,7 +6,7 @@ type Props = {
   blogs: Blog[]
 }
 
-export const BlogScreens = ({ blogs }: Props) => {
+export const BlogListScreens = ({ blogs }: Props) => {
   return (
     <main className="w-full">
       <div className="w-full flex justify-center">
