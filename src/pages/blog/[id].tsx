eb3555dc1ee38e2d07b16d0dceae63845c510100
nisitin <seiya110307@icloud.com>
2022-04-26 23:19:12 +0900

    feat: 動的ルーティングの取得

---
+++ src/pages/blog/[id].tsx
@@ -0,0 +1,37 @@
+import { GetStaticPaths } from 'next'
+import { Header } from '~/src/components/base/Header'
+import { client } from '~/src/lib/client'
+import { Blog } from '~/src/types/blog'
+
+type blogs = {
+  blog: Blog
+}
+
+// ここでpathを生成している。
+export const getStaticPaths: GetStaticPaths = async () => {
+  const data = await client.get({ endpoint: 'blog' })
+
+  const paths = data.contents.map((contents: Blog) => `/blog/${contents.id}`)
+  return { paths, fallback: false }
+}
+
+// contextはビルドする前のオブジェクトを指している。
+export const getStaticProps = async (context: any) => {
+  const id = context.params.id
+  const data = await client.get({ endpoint: 'blog', contentId: id })
+
+  return {
+    props: {
+      blog: data,
+    },
+  }
+}
+
+export default function BlogId({ blog }: blogs) {
+  return (
+    <div>
+      <Header />
+      <h1>{blog.title}</h1>
+    </div>
+  )
+}
