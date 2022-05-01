f7428871caed516392ec33c5e4681a878bd455f9
nisitin <seiya110307@icloud.com>
2022-05-01 15:23:42 +0900

    feat: propsの追加

--- src/components/screens/BlogListScreens/index.tsx
+++ src/components/screens/BlogListScreens/index.tsx
@@ -1,17 +1,23 @@
 import { Blog } from '~/src/types/blog'
 import { Card } from './components/Card'
 import { ProfileCard } from './components/ProfileCard'
+import { CategoryCard } from '~/src/components/base/CategoryCard'
+import { Category } from '~/src/types/category'
 
 type Props = {
   blogs: Blog[]
+  category: Category[]
 }
 
-export const BlogListScreens = ({ blogs }: Props) => {
+export const BlogListScreens = ({ blogs, category }: Props) => {
   return (
     <main className="w-full">
       <div className="w-full flex justify-center">
         <Card blogs={blogs} />
-        <ProfileCard />
+        <div className="w-1/5 ml-20">
+          <ProfileCard />
+          <CategoryCard category={category} />
+        </div>
       </div>
     </main>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,26 +4,30 @@ import { BlogListScreens } from '~/src/components/screens/BlogListScreens'
 import { client } from '~/src/lib/client'
 import { Blog } from '~/src/types/blog'
 import { Footer } from '../components/base/Footer'
+import { Category } from '../types/category'
 
 type Props = {
-  blogs: Blog[]
+  blog: Blog[]
+  category: Category[]
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const blog = await client.get({ endpoint: 'blog' })
+  const category = await client.get({ endpoint: 'categories' })
 
   return {
     props: {
-      blogs: blog.contents,
+      blog: blog.contents,
+      category: category.contents,
     },
   }
 }
 
-function Home({ blogs }: Props) {
+function Home({ blog, category }: Props) {
   return (
     <div>
       <Header />
-      <BlogListScreens blogs={blogs} />
+      <BlogListScreens blogs={blog} category={category} />
       <Footer />
     </div>
   )
