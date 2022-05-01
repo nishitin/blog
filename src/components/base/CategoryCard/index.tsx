06a02c412d8b23c6deb3c8a77207c0f0cb00a60f
nisitin <seiya110307@icloud.com>
2022-05-01 15:45:09 +0900

    feat: デザインを変更

--- src/components/base/CategoryCard/index.tsx
+++ src/components/base/CategoryCard/index.tsx
@@ -9,9 +9,13 @@ export const CategoryCard = memo(({ category }: Props) => {
   return (
     <div className="border-solid border-2 border-gray-400 p-8">
       <div>
-        <span>カテゴリー</span>
+        <span className="w-full bg-gray-400 px-2 py-0.5 rounded-md flex mb-2">カテゴリー</span>
         {category.map((c) => {
-          return <div key={c.id}>{c.name}</div>
+          return (
+            <p key={c.id} className="border-b px-2 border-gray-300 mb-2 cursor-pointer ">
+              {c.name}
+            </p>
+          )
         })}
       </div>
     </div>
