49d80c50c6c9b55bb16402b3e26c217d88960a27
nisitin <seiya110307@icloud.com>
2022-05-10 21:45:40 +0900

    feat: path追加

--- src/components/base/Header/index.tsx
+++ src/components/base/Header/index.tsx
@@ -13,10 +13,26 @@ export const Header = () => {
         </Link>
         <nav className="w-full flex justify-end">
           <ul className="flex mt-2 mr-20">
-            <li className="text-white ml-2 cursor-pointer">Training</li>
-            <li className="text-white ml-2 cursor-pointer">Frontend</li>
-            <li className="text-white ml-2 cursor-pointer">Backend</li>
-            <li className="text-white ml-2 cursor-pointer">Book</li>
+            <li className="text-white ml-2 cursor-pointer">
+              <Link href="/category/training">
+                <a>Training</a>
+              </Link>
+            </li>
+            <li className="text-white ml-2 cursor-pointer">
+              <Link href="/category/frontend">
+                <a>Frontend</a>
+              </Link>
+            </li>
+            <li className="text-white ml-2 cursor-pointer">
+              <Link href="/category/backend">
+                <a>Backend</a>
+              </Link>
+            </li>
+            <li className="text-white ml-2 cursor-pointer">
+              <Link href="/category/book">
+                <a>Book</a>
+              </Link>
+            </li>
             <li className="text-white ml-2 cursor-pointer">Profile</li>
           </ul>
         </nav>
