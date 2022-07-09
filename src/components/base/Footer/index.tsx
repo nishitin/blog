9237e7d95637d70286368cb506f630a50addde78
nisitin <seiya110307@icloud.com>
2022-07-09 18:13:39 +0900

    fix: footer修正

--- src/components/base/Footer/index.tsx
+++ src/components/base/Footer/index.tsx
@@ -1,3 +1,11 @@
 export const Footer = () => {
-  return <footer className="w-full h-11 bottom-0 absolute bg-gray-400"></footer>
+  return (
+    <footer className="w-full h-11 bottom-0 absolute bg-gray-400">
+      <div className="h-full">
+        <p className="h-full flex justify-center align-middle items-center text-white text-xs">
+          ©︎ 2022 nishitinBlog
+        </p>
+      </div>
+    </footer>
+  )
 }
